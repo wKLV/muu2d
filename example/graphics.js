457164150d929c842fbe6c803a5a4dd4f7ed1bcd
KLV <wkaeleuve@gmail.com>
2013-03-17 08:41:01 +0100

    First commit

---
+++ example/graphics.js
@@ -0,0 +1,106 @@
+{
+"sprite-in-open":{
+    "size":{"x": 396,"y": 406},
+    "position":{"x":5, "y":5}
+}, 
+
+"sprite-pelusaheavyeyesmouth":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":411, "y":5}
+}, 
+
+"sprite-accele":{
+    "size":{"x": 101,"y": 115},
+    "position":{"x":521, "y":5}
+}, 
+
+"sprite-atra":{
+    "size":{"x": 99,"y": 96},
+    "position":{"x":411, "y":130}
+}, 
+
+"sprite-anti":{
+    "size":{"x": 99,"y": 96},
+    "position":{"x":520, "y":130}
+}, 
+
+"sprite-door":{
+    "size":{"x": 50,"y": 55},
+    "position":{"x":632, "y":5}
+}, 
+
+"sprite-Reset":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":632, "y":70}
+}, 
+
+"sprite-pelusaheavy":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":411, "y":236}
+}, 
+
+"sprite-pelusaheavyeyes":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":521, "y":236}
+}, 
+
+"sprite-in":{
+    "size":{"x": 396,"y": 406},
+    "position":{"x":692, "y":5}
+}, 
+
+"sprite-doorgot":{
+    "size":{"x": 50,"y": 56},
+    "position":{"x":5, "y":421}
+}, 
+
+"sprite-pelusaheavymouth":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":65, "y":421}
+}, 
+
+"sprite-pelusanone":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":175, "y":421}
+}, 
+
+"sprite-pelusanoneeyes":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":285, "y":421}
+}, 
+
+"sprite-pelusanoneeyesmouth":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":395, "y":421}
+}, 
+
+"sprite-pelusanonemouth":{
+    "size":{"x": 100,"y": 97},
+    "position":{"x":505, "y":421}
+}, 
+
+"sprite-play":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":615, "y":421}
+}, 
+
+"sprite-star":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":675, "y":421}
+}, 
+
+"sprite-stargot":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":735, "y":421}
+}, 
+
+"sprite-timeplus":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":795, "y":421}
+}, 
+
+"sprite-timereg":{
+    "size":{"x": 50,"y": 50},
+    "position":{"x":855, "y":421}
+}
+}
