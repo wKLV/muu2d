fa73f8ea8c4f0faf32b76df2bc3851c6b2f30ec1
KLV <wkaeleuve@gmail.com>
2013-04-11 09:39:00 +0200

    Update examples

---
+++ example/atlas.js
@@ -0,0 +1,61 @@
+{
+"paupaso1":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":0, "y":0}
+}, 
+
+"paucarga":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":150, "y":0}
+}, 
+
+"pau":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":300, "y":0}
+}, 
+
+"pauataca":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":0, "y":150}
+}, 
+
+"pauataca2":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":150, "y":150}
+}, 
+
+"pauataca3":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":300, "y":150}
+}, 
+
+"ninjavientre":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":0, "y":300}
+}, 
+
+"paupaso2":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":150, "y":300}
+}, 
+
+"pauusa":{
+    "size":{"x": 150,"y": 150},
+    "position":{"x":300, "y":300}
+}, 
+
+"sueloatras":{
+    "size":{"x": 132,"y": 256},
+    "position":{"x":450, "y":0}
+}, 
+
+"suelodelante":{
+    "size":{"x": 103,"y": 256},
+    "position":{"x":582, "y":0}
+}, 
+
+"suelomedio":{
+    "size":{"x": 215,"y": 256},
+    "position":{"x":450, "y":256}
+}
+}
